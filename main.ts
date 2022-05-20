9aea6152e49a9e62d179880222ca489f7f329393
iAndrrei <97289654+iAndrrei@users.noreply.github.com>
2022-05-20 11:48:20 +0300

    Restore '06/05/2022, 11:40:56 Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
-let sonar = 0
 basic.showIcon(IconNames.Ghost)
-music.playMelody("C D E F G A B C5 ", 120)
+music.playMelody("- - - - - - - - ", 120)
 basic.forever(function () {
-    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-})
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-    if (input.lightLevel() < 10) {
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 200, 200, 200)
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 200, 200, 200)
-    } else {
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 0, 0, 0)
-        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
-    }
+    cuteBot.forward()
+    basic.pause(200)
+    cuteBot.motors(100, 40)
     basic.pause(1000)
+    cuteBot.forward()
+    basic.pause(200)
+    cuteBot.forward()
+    basic.pause(200)
+    cuteBot.motors(40, 100)
+    basic.pause(1000)
+    cuteBot.forward()
+    basic.pause(200)
 })
