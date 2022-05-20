a0f91e9d622041a90e34a02bd9c6305098188bb5
iAndrrei <97289654+iAndrrei@users.noreply.github.com>
2022-05-20 11:34:44 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
+let sonar = 0
 basic.showIcon(IconNames.Ghost)
-music.playMelody("- - - - - - - - ", 120)
+music.playMelody("C D E F G A B C5 ", 120)
 basic.forever(function () {
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(100, 40)
+    sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (input.lightLevel() < 10) {
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 200, 200, 200)
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 200, 200, 200)
+    } else {
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 0, 0, 0)
+        cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 0)
+    }
     basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(40, 100)
-    basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
 })
